82035759751e0d953bf0c28fa61ccc9d6544e36f
Maria <marry090907@gmail.com>
2025-10-17 20:53:18 -0300

    fix: atualizando o código do semafaro

--- Aula1/semafaro.ino
+++ Aula1/semafaro.ino
@@ -1,19 +1,23 @@
 void setup()
 {
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   pinMode(11, OUTPUT);
+  pinMode(9, OUTPUT);
 }
 
 void loop()
 {
-  digitalWrite(9, HIGH);
-  delay(3000); 
-  digitalWrite(9, LOW);
-  digitalWrite(10, HIGH);
-  delay(500); 
-  digitalWrite(10, LOW);
+  digitalWrite(13, HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(2000); // Wait for 2000 millisecond(s)
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(13, LOW);
   digitalWrite(11, HIGH);
-  delay(2000); 
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(LED_BUILTIN, HIGH);
   digitalWrite(11, LOW);
+  digitalWrite(9, HIGH);
+  delay(2000); // Wait for 2000 millisecond(s)
+  digitalWrite(9, LOW);
 }
